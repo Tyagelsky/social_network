13591412f4c271824e247f452139f0a5710e2cba
Yaroslav <tyagelsky@yandex.ru>
2017-04-20 00:38:12 +0300

    added missed validation fot user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+  validates :username, presence: true
   has_many :articles , dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed
